392d5d49c265ff24523b4eadeaf6fc2d5effc111
Mohammad Rafiq <googaabumtum@gmail.com>
2020-01-13 22:49:05 +0800

    added boxes to game.
    will spawn boxes of random size and xpos.

---
+++ FYP2-2020/Assets/Scripts/GameController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GameController : MonoBehaviour
+{
+    public bool shouldTimerRun = true;
+    public Timer timer;
+    public Camera mainCamera;
+    public GameObject platformPrefab;
+    public List<GameObject> platforms;
+    public float platformDropSpeed = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (platforms.Count == 0)
+        {
+            StartCoroutine(SpawnPlatform());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldTimerRun)
+            timer.UpdateTimerText();
+    }
+
+    private void FixedUpdate()
+    {
+        UpdatePlatforms();
+    }
+
+    private void UpdatePlatforms()
+    {
+        foreach (GameObject platform in platforms)
+        {
+            if (platform.transform.position.y < -10f)
+            {
+                Debug.Log("Destroying platform.");
+                platforms.Remove(platform);
+                Destroy(platform);
+                continue;
+            }
+            platform.transform.Translate(0f, -platformDropSpeed, 0f);
+
+            float highestPlatformPosition = -100000f;
+            if (platform.transform.position.y >= highestPlatformPosition)
+            {
+                highestPlatformPosition = platform.transform.position.y;
+                Debug.Log("Highest platform now at " + highestPlatformPosition);
+            }
+
+            if (highestPlatformPosition <= -5 && platforms.Count < 2f)
+            {
+                StartCoroutine(SpawnPlatform());
+            }
+        }
+
+    }
+
+    IEnumerator SpawnPlatform()
+    {
+        Debug.Log("Spawning platform.");
+
+        float platformScaleX = Random.Range(2.5f, 5f);
+        float platformEdge = Random.Range(-1f, 1f);
+        float platformPosX = 0f;
+        if (platformEdge < 0f)
+            platformPosX = platformEdge + platformScaleX;
+        else if (platformEdge > 0f)
+            platformPosX = platformEdge - platformScaleX;
+        else
+            platformPosX = platformEdge;
+
+        GameObject newPlatform = Instantiate(platformPrefab);
+        newPlatform.transform.position = new Vector3(platformPosX, 0f, 28.75f);
+        newPlatform.transform.localScale = new Vector3(platformScaleX, 1f, 2.5f);
+        platforms.Add(newPlatform);
+        yield return 0;
+    }
+}
---
+++ FYP2-2020/Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    private float timeElapsed = 0f;
+    void Update()
+    {
+
+    }
+
+    public void UpdateTimerText()
+    {
+        //set timer UI
+        timeElapsed += Time.deltaTime;
+        timerText.text = timeElapsed.ToString();
+    }
+}
