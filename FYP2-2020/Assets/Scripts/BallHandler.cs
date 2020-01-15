480c4ea4b550154eab91c9f442fad8336b2f45a8
Mohammad Rafiq <googaabumtum@gmail.com>
2020-01-15 21:59:02 +0800

    bruh

---
+++ FYP2-2020/Assets/Scripts/BallHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallHandler : MonoBehaviour
+{
+    public float offset = 1f;
+
+    private float initialMouseXPos;
+    private float initialBallXPos;
+    private bool mouseDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseDown = true;
+            initialMouseXPos = Input.mousePosition.x;
+            initialBallXPos = transform.position.x;
+        }
+
+        if (mouseDown && Input.GetMouseButtonUp(0))
+            mouseDown = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0) && mouseDown)
+        {
+            transform.position = new Vector3(initialBallXPos + (Input.mousePosition.x - initialMouseXPos) * offset, transform.position.y, transform.position.z);
+        }
+    }
+
+}
